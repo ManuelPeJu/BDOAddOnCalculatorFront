68d19f5e83a9f320a443a7039dcd860028f0c854
Manuel Pérez Jurado <perezjuradomanuel@gmail.com>
2023-11-30 19:42:01 +0100

    sweetAlerts install

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,17 @@ import NavBar from "./components/Nav/NavBar"
 function App() {
   return (
     <>
-      <NavBar />
-      <h1>Hola mundo</h1>
+      <header>
+        <NavBar />
+      </header>
+      <section className="container-fluid hero">
+        <h3>Hello adventurer!!</h3>
+        <p>We are a team of 3 persons that play and love Black Desert Online.</p>
+        <h4>What is our objective ?</h4>
+        <p>Our objective is create a calculator to maximize your grind time in the spot you want, and get a satisfying and lucrative grinding session to reach your goals easier and faster.</p>
+        <p>To do this, we think that Addons is the perfect way to reach this goal.</p>
+      </section>
+      
     </>
   )
 }
