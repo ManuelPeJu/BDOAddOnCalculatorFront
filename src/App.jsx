f9ce61a37ff3bf87eeca7fa7e5b74ffcdbfc6f61
Manuel Pérez Jurado <perezjuradomanuel@gmail.com>
2023-12-01 02:25:50 +0100

    hero created and some text to fill and test css

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import NavBar from "./components/Nav/NavBar"
+import NavBar from "./components/Nav/NavBar";
+import Hero from "./components/Hero/Hero";
 
 function App() {
   return (
@@ -6,13 +7,7 @@ function App() {
       <header>
         <NavBar />
       </header>
-      <section className="container-fluid hero">
-        <h3>Hello adventurer!!</h3>
-        <p>We are a team of 3 persons that play and love Black Desert Online.</p>
-        <h4>What is our objective ?</h4>
-        <p>Our objective is create a calculator to maximize your grind time in the spot you want, and get a satisfying and lucrative grinding session to reach your goals easier and faster.</p>
-        <p>To do this, we think that Addons is the perfect way to reach this goal.</p>
-      </section>
+      <Hero />
       
     </>
   )
