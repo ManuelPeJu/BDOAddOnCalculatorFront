f27ebcd4fa946916599c46c154d1f9d3131970c2
Manuel Pérez Jurado <perezjuradomanuel@gmail.com>
2023-12-12 19:30:36 +0100

    redux and some redux testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
 // import Theme from "./components/Theme/Theme"
 import AboutUs from "./components/AboutUs/AboutUs";
 import ThreeSections from "./components/Sections/Sections";
 import HowToUse from "./components/HowToUse/HowToUse";
-
+import { addUser } from "./Redux/userSlice";
 function App() {
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users/1")
+      .then((response) => response.json())
+      .then((data) => dispatch(addUser(data)))
+      .catch((error) => console.log(error))
+   }, [])
 
   return (
     <>
