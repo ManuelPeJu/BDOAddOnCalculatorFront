e4cc2fa9c59524e4d6e9fca83895881f9f0feeff
Manuel Pérez Jurado <perezjuradomanuel@gmail.com>
2023-12-19 19:59:01 +0100

    get api info choosing weapon state

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
+import React from "react";
 import AboutUs from "./components/AboutUs/AboutUs";
 import ThreeSections from "./components/Sections/Sections";
 import HowToUse from "./components/HowToUse/HowToUse";
-import { addUser } from "./Redux/userSlice";
 import ThreeSectionsTest from "./components/Sections/sectionsTEST";
 import ScrollToTopButton from "./components/ScrollToTop/ScrollToTop";
+import Footer from "./components/Footer/Footer";
 function App() {
 
   return (
@@ -18,7 +17,7 @@ function App() {
       <ScrollToTopButton /> 
       </main>
       <footer>
-        IM NOT A FOOTER
+        <Footer />
       </footer>
     </>
   )
