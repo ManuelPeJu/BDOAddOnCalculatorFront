7c68d4f7d09cf6e10d1f065d77a76fe6401b096c
Manuel Pérez Jurado <perezjuradomanuel@gmail.com>
2023-12-21 19:19:31 +0100

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,9 +2,38 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='fluid-container footer'>
-      <h1>Made with ❤️</h1>
-    </footer>
+<footer className='fluid-container'>
+  <div class="logo">
+    <img src="../../assets/logo_TEMP.png" alt="Logo" />
+  </div>
+  {/* YISUS */}
+  <div class="redes-sociales">
+    <div className='list-item'>
+        <span>Snowfall86</span>
+        <p>thanks our programmer Jesús for helping to create this app</p>
+    </div>
+  </div>
+  {/* GAMERURC */}
+  <div class="redes-sociales">
+      <div className='list-item'>
+        <span>GameruRc</span>
+        <a href="https://www.twitch.tv/gamerurc"><i class="bi bi-cast"></i></a>
+      </div>
+      <div className='list-item'>
+        <a href="https://www.youtube.com/@GameruRc"><i class="bi bi-youtube"></i></a>
+      </div>
+  </div>
+  {/* XDUIM */}
+  <div class="redes-sociales">
+      <div className='list-item'>
+        <span>xDuim</span>
+        <a href="https://github.com/ManuelPeJu"><i class="bi bi-github"></i></a>
+      </div>
+      <div className='list-item'>
+        <a href="https://www.linkedin.com/in/manuel-pérez-jurado/"><i class="bi bi-linkedin"></i></a>
+      </div>
+  </div>
+</footer>
   )
 }
 
